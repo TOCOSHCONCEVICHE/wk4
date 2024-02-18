aed359162b22e1ce4f59bcee4af0ef9428cb74bb
mjsanchez@york.edu <mjsanchez@york.edu>
2024-02-18 16:14:05 -0600

    npm init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('It worked!');
+import { Vehicle } from './vehicle';
+import { Car } from './car';
+import { Motorcycle } from './motorcycle';
+
+let myCar = new Car(4, 'Hyundai', 'Sports');
+let myMotorcycle = new Motorcycle(2, "BMW", true, 'Cruiser');
+
+myCar.getInfo();
+myCar.getType('Ronald');
+myCar.getWheels();
+myMotorcycle.getInfo();
+myMotorcycle.getWheels();
+myMotorcycle.isItSafe();
+
